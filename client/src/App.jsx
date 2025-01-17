a62ff98effcba6e9a7a912f6fa55b28c52ce32be
ahladshetty <ahladshetty03@gmail.com>
2025-01-17 21:18:33 +0530

    setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,35 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import "./App.css";
-import Login from "./components/Login";
-import Home from "./components/Home";
-import Vote from "./components/Vote";
-import Menu from "./components/Menu";
-import Signup from "./components/Signup";
-import Account from "./components/Account";
-import StaffLogin from "./components/StaffLogin";
-
-import EditMenu from "./components/EditMenu";
-import ProtectedRoute from "./components/ProtectedRoute";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Login />} />
-        <Route exact path="/stafflogin" element={ <StaffLogin />} />
-        <Route exact path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-        <Route exact path="/menu" element={<ProtectedRoute><Menu /></ProtectedRoute>} />
-        <Route exact path="/vote" element={<ProtectedRoute><Vote /></ProtectedRoute>} />
-        <Route exact path="/signup" element={<Signup />} />
-        <Route exact path="/about" element={<Account />} />
-        <Route exact path="/editmenu" element={<EditMenu />} />
-      </Routes>
-    </Router>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
-import { AppProvider } from './context/appContext.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AppProvider>
-
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-
-    </AppProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 )
